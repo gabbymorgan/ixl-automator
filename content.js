0a0b34bdc4e83e74c56d8a3e7947641c847acdbf
William Pelton <williamrpelton@gmail.com>
2019-04-09 11:30:24 -0500

    ten minute breaks for each 20 questions attempted

--- content.js
+++ content.js
@@ -1,14 +1,57 @@
 
 var waitOneMoment;
-var choiceNodeClassNames = ['TileSkinBare', 'SelectableTile', 'numberButton'];
+var choiceNodeClassNames = ['TileSkinBare', 'SelectableTile', 'numberButton', "TileSkinClassic"];
 var startWaiting = () => {
   waitOneMoment = setTimeout(() => {
     document.getElementsByClassName('crisp-button')[3].click();
   }, 10000);
-}
+};
+
 var stopWaiting = () => {
   clearTimeout(waitOneMoment);
-}
+};
+
+// must explicitly apply each value because of DOM strangeness
+var applyAttrs = (object, attrs) => {
+  Object.keys(attrs).forEach(attribute => {
+    object[attribute] = attrs[attribute];
+  });
+};
+
+var checkForBreak = () => {
+  // const score = document.getElementById('currentscore').innerText;
+  const attempts = document.getElementById('problems-attempted').innerText;
+  console.log({ attempts });
+  if (attempts % 20 === 0) {
+    const takeBreak = document.createElement('div');
+    takeBreakStyles = {
+      width: "100vw",
+      height: "100vh",
+      backgroundColor: "rgba(100, 100, 100, .5)",
+      position: "absolute",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    };
+    applyAttrs(takeBreak.style, takeBreakStyles);
+    const iframeAttrs = {
+      width: "724",
+      height: "407",
+      src: "https://www.youtube.com/embed/u5dVwQp4liA?autoplay=1",
+      frameborder: "0",
+      allow: "autoplay;",
+      autoplay: true,
+    }
+    const breakTimer = document.createElement('iframe');
+    applyAttrs(breakTimer, iframeAttrs);
+    takeBreak.appendChild(breakTimer);
+    document.body.appendChild(takeBreak);
+    setTimeout(() => {
+      takeBreak.remove();
+    }, 1000 * 60 * 10.25);
+  }
+};
+
 var init = () => {
   for (let i = 0; i < choiceNodeClassNames.length; i++) {
     const choiceNodes = Array.from(document.getElementsByClassName(choiceNodeClassNames[i]));
@@ -25,13 +68,15 @@ var init = () => {
   if (submitButton) {
     submitButton.addEventListener("click", () => {
       stopWaiting();
+      console.log('submit')
+      checkForBreak();
     });
   }
-}
+};
 
 var clockWatcher = new MutationObserver(function (mutations) {
   if (Array.from(mutations[0].target.classList).includes("timer-paused")) {
-    alert("Times up!");
+    checkForBreak();
   }
 });
 
